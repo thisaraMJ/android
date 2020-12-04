55410d01537e79c9179c330ea9fcc2b2ac4c89a1
thisaraMJ <madushantjg@gmail.com>
2020-12-04 11:06:15 +0530

    login module initial commit

---
+++ login module/dbConfig.php
@@ -0,0 +1,21 @@
+<?php
+
+class dbConfig
+{
+    public $servername;
+    public $username;
+    public $password;
+    public $databasename;
+
+    public function __construct()
+    {
+
+        $this->servername = 'localhost';
+        $this->username = 'root';
+        $this->password = '';
+        $this->databasename = 'project';
+
+    }
+}
+
+?>
