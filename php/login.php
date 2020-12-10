1aba5b1c30525b3190df9296809d4cf11fb28730
thisaraMJ <madushantjg@gmail.com>
2020-12-11 01:29:39 +0530

    updation

--- php/login.php
+++ php/login.php
@@ -5,7 +5,7 @@ if (isset($_POST['email']) && isset($_POST['password'])) {
     if ($db->dbConnect()) {
         if ($db->logIn("users", $_POST['email'], $_POST['password'])) {
             echo "Login Success";
-        } else echo "Username or Password wrong";
+        } else echo "email or Password wrong";
     } else echo "Error: Database connection";
 } else echo "All fields are required";
 ?>
