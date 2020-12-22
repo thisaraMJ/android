2401067d210c3453ba93a46f4d02b1ff765634fa
thisaraMJ <madushantjg@gmail.com>
2020-12-23 01:04:28 +0530

    updation 12/23

--- app/src/main/java/com/example/testlogin/MainActivity.java
+++ app/src/main/java/com/example/testlogin/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.testlogin;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final DrawerLayout drawerLayout = findViewById(R.id.drawerLayout);
+
+        findViewById(R.id.imageMenu).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
     }
 }
