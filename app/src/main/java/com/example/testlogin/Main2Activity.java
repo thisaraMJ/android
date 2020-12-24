0e184cd59ff3111813890e39a317fafe96aa29d0
thisaraMJ <madushantjg@gmail.com>
2020-12-25 00:42:47 +0530

    updation 12/25

--- app/src/main/java/com/example/testlogin/Main2Activity.java
+++ app/src/main/java/com/example/testlogin/Main2Activity.java
@@ -1,20 +1,14 @@
 package com.example.testlogin;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 
-
-
-public class MainActivity extends AppCompatActivity {
-
+public class Main2Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-
-
-
+        setContentView(R.layout.activity_main2);
     }
 }
